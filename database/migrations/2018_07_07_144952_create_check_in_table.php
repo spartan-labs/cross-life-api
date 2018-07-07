5b10d551dda9eaf2665b9678c740d596514112be
Lucas Gonçalves <lucaslucasfabio@hotmail.com>
2018-07-07 12:05:55 -0300

    add new migrations

--- database/migrations/2018_07_07_144952_create_check_in_table.php
+++ database/migrations/2018_07_07_144952_create_check_in_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClassesTable extends Migration
+class CreateCheckInTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateClassesTable extends Migration
      */
     public function up()
     {
-        Schema::create('classes', function (Blueprint $table) {
+        Schema::create('check_in', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->text('description');
-            $table->integer('personal_id')->unsigned()->nullable();
-            $table->foreign('personal_id')->references('id')->on('persons');
+            $table->integer('class_id')->unsigned()->nullable();
+            $table->foreign('class_id')->references('id')->on('classes');
+            $table->integer('person_id')->unsigned()->nullable();
+            $table->foreign('person_id')->references('id')->on('persons');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateClassesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('classes');
+        Schema::dropIfExists('check_in');
     }
 }
